142250da457ce4c70c6affde0b9edaa2b49a807c
Joshua <joridinsolo@gmail.com>
2017-07-27 02:47:27 +0400

    Add varying pixels and opacity

--- scripts/script-main.js
+++ scripts/script-main.js
@@ -1,25 +1,51 @@
 $(document).ready(function() {
   var $box = $('#box');
   var totalHeight = 700;
-  var pixels = 100;
-  for (var i = 0; i < pixels*pixels; i++) {
-    $box.append('<div class="pixel"></div>');
+  create(10);
+  $('#enter').on('click', function() {
+    clean();
+    pixels = $(this).parent().find('#pixel-input').val();
+    create(pixels);
+
+  });
+
+
+
+  function create(pixels) {
+    for (var i = 0; i < pixels*pixels; i++) {
+      $box.append('<div class="pixel"></div>');
+    }
+    var $pixel = $('.pixel');
+    var height = totalHeight/pixels;
+    var margin = height*.01;
+    var height = height - (2*margin);
+    $pixel.height(height).width(height);
+    $pixel.css('margin', margin);
+    $pixel.show();
+
+    $('.pixel').on('mouseenter', function() {
+      $(this).addClass('touched');
+      var value = parseFloat($(this).css("opacity"));
+        if(value<1){
+          value += 0.2;
+          $(this).css("opacity", value);
+        }
+    });
+
+    $('#clear').on('click', function() {
+      $pixel.removeClass('touched');
+    });
+
+
   }
 
-  var $pixel = $('.pixel');
-  var height = totalHeight/pixels;
-  var margin = height*.01;
-  var height = height - (2*margin);
-  $pixel.height(height).width(height);
-  $pixel.css('margin', margin);
-  $pixel.show();
-  $pixel.on('mouseenter', function() {
-    $(this).addClass('touched');
-  });
+  function clean() {
+    var $parent = $('.pixel').parent();
 
-  $('#clear').on('click', function() {
-    $pixel.removeClass('touched');
-  });
+    $('.pixel').removeClass('pixel').removeClass('touched');
+    $('.pixel').removeClass('touched');
+    $parent.empty();
+  }
 
 
 });
