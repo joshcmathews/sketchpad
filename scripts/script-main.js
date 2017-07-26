5ea8d767bc4eb4615839ba509c195f8e75ee43d9
Joshua <joridinsolo@gmail.com>
2017-07-27 01:13:42 +0400

    Add sketchpad functionality

---
+++ scripts/script-main.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+  var $box = $('#box');
+  var totalHeight = 700;
+  var pixels = 10;
+  for (var i = 0; i < pixels*pixels; i++) {
+    $box.append('<div class="pixel"></div>');
+  }
+
+  var $pixel = $('.pixel');
+  var height = totalHeight/pixels;
+  var margin = height*.01;
+  var height = height - (2*margin);
+  $pixel.height(height).width(height);
+  $pixel.css('margin', margin);
+  $pixel.show();
+  $pixel.on('mouseenter', function() {
+    $(this).addClass('touched');
+  });
+
+  $('#clear').on('click', function() {
+    $pixel.removeClass('touched');
+  });
+
+
+});
